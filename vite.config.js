ad4b0547ed7e23e50fb54b8bc18298fb6cf9f485
youssef shafek <youssefmsa616@gmail.com>
2024-08-02 01:53:33 +0300

    Signed-off-by: youssef shafek <youssefmsa616@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-task1/',
   plugins: [react()],
-  server: {
-    historyApiFallback: true,
-  },
 })
-
